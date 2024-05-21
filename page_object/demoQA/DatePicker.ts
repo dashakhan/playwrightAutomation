9b536672e0b8a8de2c66c7885c0fdd6f11ab4f6e
dash <89936740+dashakhan@users.noreply.github.com>
2024-05-21 17:21:32 -0400

    updates (#6)

--- page_object/demoQA/DatePicker.ts
+++ page_object/demoQA/DatePicker.ts
@@ -1,5 +1,5 @@
-import {Page, expect} from "@playwright/test"
-
+import {Page, expect, test as it} from "@playwright/test"
+import _ from "lodash";
 
 export class DatePicker{
     public readonly page: Page
@@ -11,11 +11,40 @@ export class DatePicker{
     get header(){
         return 'h1[class="text-center"]'
     }
+    get formInput(){
+        return '[id="datePickerMonthYearInput"]'
+    }
+
+    get componentAllDates(){
+        return '[class="react-datepicker__month-container"]'
+    }
+
+    get prevMonth(){
+        return '[aria-label="Previous Month"]'
+    }
+    get dateComponent(){
+        return '[role="option"]'
+    }
 
     public async verifyHeader(){
         const header = this.page.locator(this.header)
         await expect(header).toContainText('Date Picker')
+    }
 
+    
+    randomNumber = _.random(1, 50);
+    date = _.random(15, 20);
+
+    public async selectDate(){
+        await this.page.locator(this.formInput).click()
+
+        for(let i = 0;i <= this.randomNumber;i++){
+            await this.page.locator(this.prevMonth).click()
+        }
+        console.log(this.randomNumber);
+    await this.page.locator('[class="react-month"]')
+    .getByText(this.date)
+    // .getByRole('option', {name: '20', exact: true}).click()
     }
 
 }
