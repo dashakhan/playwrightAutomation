fffd70ade5a72770cecfe51fc5e513d8275cd0ec
DashaGKH <keshonti2016@gmail.com>
2024-05-22 10:37:14 -0700

    drag n drop elective

--- tests/demoQa/interactions/dragAndDropDemo.spec.ts
+++ tests/demoQa/interactions/dragAndDropDemo.spec.ts
@@ -1,5 +1,5 @@
 import {test as it} from "@playwright/test"
-import { DragAndDropDemo } from "../../page_object/demoQA/DragAndDropDemo"
+import { DragAndDropDemo } from "../../../page_object/demoQA/DragAndDropDemo"
 
 
 
@@ -7,7 +7,7 @@ it.describe('DRAG AND DROP FRO DEMO QA', ()=>{
     it('drag and drop + css', async ({page})=>{
         let dragAndDropDemo = new DragAndDropDemo(page)
         await page.goto('https://demoqa.com/droppable#google_vignette')
-        await dragAndDropDemo.dragAndDropDate('Drag me')
+        await dragAndDropDemo.dragAndDropDate()
         await dragAndDropDemo.dragAndDropAccept('Acceptable')
         await dragAndDropDemo.dragAndDropAccept('Not Acceptable')
     })
