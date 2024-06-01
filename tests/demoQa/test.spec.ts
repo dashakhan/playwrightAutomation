4aa1b273a90f4026dc1ee78ef23b0bd9ad253bc3
dash <89936740+dashakhan@users.noreply.github.com>
2024-06-01 14:48:37 -0400

    auth for pasv (#13)

--- tests/demoQa/test.spec.ts
+++ tests/demoQa/test.spec.ts
@@ -1,17 +1,31 @@
+// import { test, expect } from "@playwright/test"
+
+
+// test.beforeEach(async ({page}) => {
+//     await page.goto('https://demoqa.com/login')
+// })
+
+// test('authentication through UI', async ({page, browserName}) =>{
+//     const name = page.locator('#submit');
+//     await expect(name).toHaveText('Log out')
+//     await page.screenshot({path: `screenShot/${browserName}.png`})
+
+// })
+
 import { test, expect } from "@playwright/test"
 
 
 test.beforeEach(async ({page}) => {
-    await page.goto('https://demoqa.com/login')
+    await page.goto('https://coding.pasv.us/profile/61003da5224eb8003a068f95')
 })
 
 test('authentication through UI', async ({page, browserName}) =>{
-    const name = page.locator('#submit');
-    await expect(name).toHaveText('Log out')
+    await page.locator('[data-icon="down"]').nth(0).click();
+    const name = page.locator('[data-qa="logout"]');
+    await expect(name).toHaveText('Logout')
     await page.screenshot({path: `screenShot/${browserName}.png`})
 
 })
-
 
 
 
