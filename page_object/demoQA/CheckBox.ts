da824872f4a21c0b896096a14e32b9fdfb1e1ab6
dash <89936740+dashakhan@users.noreply.github.com>
2024-06-03 22:31:29 -0400

    fixture for checkBox (#14)

---
+++ page_object/demoQA/CheckBox.ts
@@ -0,0 +1,34 @@
+import { Page } from "@playwright/test";
+
+export class CheckBox {
+  readonly page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  get expandToggle() {
+    return '[class="rct-icon rct-icon-expand-close"]';
+  }
+  get subDirectoryCBox() {
+    return '[class="rct-icon rct-icon-uncheck"]';
+  }
+  get checkedBox(){
+      return '[class="rct-icon rct-icon-check"]'
+  }
+
+  async goto() {
+    await this.page.goto("https://demoqa.com/checkbox");
+  }
+
+  async expandOptions(): Promise<void> {
+    while ((await this.page.locator(this.expandToggle).count()) > 0) {
+        await this.page.locator(this.expandToggle).first().click()
+    }
+  }
+
+  async checkAll(){
+    await this.page.locator(this.subDirectoryCBox).first().check()
+    // await this.page.locator('input[type=checkbox]').first().check()
+  }
+
+}
